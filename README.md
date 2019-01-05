f9d13de7d67cf3f7d5c7b39e894354dfe30d5588
Konstantin <konstantin.v.modin@gmail.com>
2019-01-05 08:39:05 +0100

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# AJAX-Exchange-Calculator
+
+Very simple example of AJAX technology on currency exchange calculator. 
+Promises included.
---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+    let request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+    
+    let promise = new Promise(function(resolve, reject) {
+        request.addEventListener('load', function() {
+            if (request.readyState == 4 && request.status == 200) {
+                resolve();                
+            } else {
+                reject();
+            }
+        });
+    });
+
+    promise
+        .then(() => {
+            let data = JSON.parse(request.response);
+            inputUsd.value = inputRub.value / data.usd;
+        })
+        .catch(() => {
+            inputUsd.value = "Что-то пошло не так!";
+        });
+    
+
+});
